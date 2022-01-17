42768ddb73d51a825cf4f009826c2c86d219ad9f
highway92 <highway@highway92ui-MacBookAir.local>
2022-01-18 08:54:08 +0900

    스택을이용한큐구현

---
+++ 스택,큐/232스택을이용한큐구현.py
@@ -0,0 +1,48 @@
+class MyQueue:
+
+    def __init__(self):
+        self.q = []
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        return self.q.pop(0)
+
+    def peek(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        if len(self.q) == 0:
+            return True
+        else:
+            return False
+
+
+class MyQueue:
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x):
+        self.input.append(x)
+
+    def pop(self):
+        self.peek()
+        return self.output.pop()
+
+    def peek(self):
+        # output이 없으면 모두 재입력
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+    
+    def empty(self):
+        return self.input == [] and self.output == []
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
