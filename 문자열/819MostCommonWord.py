eab117f389dec8f81b068c4870fdf0854fe80fd8
highway92 <highway92@kakao.com>
2022-01-31 21:45:24 +0900

    update string section

---
+++ 문자열/819MostCommonWord.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+        .lower().split()
+            if word not in banned]
+        
+        counts = Counter(words)
+        return counts.most_common(1)[0][0]
